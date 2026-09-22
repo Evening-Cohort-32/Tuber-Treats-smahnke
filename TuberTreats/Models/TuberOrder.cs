83ca2bcd1231ce2b10e143cab909853ab5f1864d
Stephanie Mahnke <steph.mahnke@gmail.com>
2026-09-21 21:18:18 -0500

    add classes

--- TuberTreats/Models/TuberOrder.cs
+++ TuberTreats/Models/TuberOrder.cs
@@ -1,1 +1,10 @@
 namespace TuberTreats.Models;
+public class TuberOrder
+{
+    public int Id { get; set; }
+    public DateOnly OrderPlacedOnDate { get; set; }
+    public int CustomerId { get; set; }
+    public int? TuberDriverId { get; set; }
+    public DateOnly DeliveredOnDate { get; set; }
+    public List<Topping> Toppings { get; set; }
+}
